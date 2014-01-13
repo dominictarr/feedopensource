e77a240beeb7c4397ced512078d10ee6e61d9ee6
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-13 22:30:30 +0700

    simple tests

---
+++ test/comments.js
@@ -0,0 +1,14 @@
+var tape = require('tape')
+
+var funders = require('../funders')
+
+var comments = require('./fixtures/comments.json')
+var wallet   = require('./fixtures/wallet.json')
+var expected = require('./fixtures/funders.json')
+
+tape('find funders', function (t) {
+  //console.log(JSON.stringify(funders(comments, wallet), null, 2))
+  var actual = funders(comments, wallet)
+  t.deepEqual(actual, expected)
+  t.end()
+})
