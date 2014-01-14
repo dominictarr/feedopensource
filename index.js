da77041771bb252bb0a0ba73840d6c5e7b127b94
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-15 02:32:14 +0700

    generate the routes, do not hard code them

--- index.js
+++ index.js
@@ -4,38 +4,30 @@ var https       = require('https')
 var http        = require('http')
 var fs          = require('fs')
 var join        = require('path').join
+
 var btcprogress = require('btcprogress')
 var route       = require('tiny-route')
-var path        = require('path')
 var ecstatic    = require('ecstatic')
 var stack       = require('stack')
+
 var redirect    = require('./lib/https-redirect')
 var config      = require('./config')
-var fos         = require('./lib')
+var api         = require('./api')
+var views       = require('./views')
 
-var bar         = btcprogress()
-
-var layout      = require('./views/layout')
+var bar  = btcprogress()
+var autoApi = require('./lib/auto-api')
 
 var app = stack(
   route('/badge/', bar),
-  route(/\/iteration\/(1[0-z]{33})/, function (req, res, next) {
-    var user = 'dominictarr'
-    var repo = 'feedopensource'
-    fos.iteration(user, repo, req.params[0], function (err, data) {
-      if(err) return next(err)
-      fos.funders(user, repo, ''+data.number, req.params[0], function (err, funders) {
-        if(err) return next(err)
-        res.end(layout(funders, require('./views/funders')))
-      })
-    })
-  }),
-  ecstatic(path.join(__dirname, 'static'))
+  autoApi(api, views),
+  ecstatic(join(__dirname, 'static'))
 )
 
 var secure = process.getuid() === 0
 
 if(secure) {
+
   https.createServer({
    cert: fs.readFileSync(config.cert),
    key : fs.readFileSync(config.key)
