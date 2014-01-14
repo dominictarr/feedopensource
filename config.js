19fecc25c70ab3aac7ab4d479ea3d0d80d14dbee
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-14 16:02:48 +0700

    default to the SUDO_USER's home dir

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 var join = require('path').join
 
+var home = process.env.SUDO_USER
+           ? join('/home', process.env.SUDO_USER)
+           : process.env.HOME
+
 module.exports = require('rc')('feedopensource', {
-  cert: join(process.env.HOME, '.feedopensource', 'keys', 'server-cert.pem'),
-  key : join(process.env.HOME, '.feedopensource', 'keys', 'server-key.pem'),
+  cert: join(home, '.feedopensource', 'keys', 'server-cert.pem'),
+  key : join(home, '.feedopensource', 'keys', 'server-key.pem'),
+  port: 8000 //development (non-secure) port
 })
